81b0891bc7509e6397b54bbcb63d24c37f8c029f
Nathnael45 <natipkmn2@gmail.com>
2024-10-03 20:48:26 -0400

    finishing touches

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"N9z5c3Dzj8yXvXoTs/FLTbFd63kSpCvIY5cXoAsjcNw=\"\n}"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,46 @@ export default function Home() {
               alt="Profile Picture"
               width={300}
               height={300}
-              className="rounded-full"
+              className="rounded-full transform transition-transform duration-300 hover:translate-x-4 hover:translate-y-4 hover:scale-110"
             />
           </div>
-          {/* Text  */}
+          {/* Info  */}
           <div className="lg:w-2/3 lg:pl-16">
             <h1 className="text-4xl font-bold mb-4">Hi, my name is Nathnael Tesfaw!</h1>
-            <p className="text-lg leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed lorem lacus. Aenean vulputate ligula posuere convallis ornare. Suspendisse ipsum ligula, maximus et nunc a, mollis malesuada magna. Phasellus vitae sagittis diam, eu ornare turpis. Sed vel finibus nunc, ut rutrum eros. Vivamus iaculis non ante a ultrices. In convallis scelerisque ullamcorper. Nulla sem mauris, imperdiet non urna vel, vestibulum sollicitudin diam. Duis metus mauris, bibendum ac nulla eget, ornare suscipit sapien.
-            </p>
+            <h2 className="text-2xl font-bold mb-4">Projects</h2>
+            <h3></h3>
+            <div className="text-lg leading-relaxed">
+             <a className  = "underline" href="https://github.com/Nathnael45/WorkoutHub"> <span>Workout Hub</span></a>
+             <br />
+             <br />
+              <a className  = "underline" href="https://github.com/dvala041/Lost_And_Found"> <span>Lost and Found App</span></a>
+              <br />
+              <br />
+              <a className  = "underline" href="https://github.com/Nathnael45/Space-Game"> <span>Space Game</span></a>
+
+            </div>
+            <br />
+            <h2 className="text-2xl font-bold mb-4">Links</h2>
+            <div className="flex">
+            <a href="https://www.linkedin.com/in/nathnael-tesfaw-2aa9171a1/"><Image
+              src= "/lockedin.png"
+              alt=" Linkdin Logo"
+              width={100}
+              height={50}
+               /></a>
+
+              <a href="https://github.com/Nathnael45"><Image
+              src= "/gothub.png"
+              alt=" Github Logo"
+              width={100}
+              height={50}
+               /></a>
+              
+            </div>
+            
+    
+
+            <p></p>
           </div>
         </div>
       </div>
